55dc95883abf2b24950d5b4f9d73c4160aae2a1f
precociouslydigital <fellfromatree@gmail.com>
2015-06-30 14:23:15 -0700

    Revert "It is BOOTS"
    This reverts commit 4c294b1ae2d0e92dd7daa6ea5a111c8f2dbd9d90.

--- entities.js
+++ entities.js
@@ -92,7 +92,7 @@ var EnemyEntity = me.ObjectEntity.extend({
     return false;
   }
 });
-var BootsEntity = me.CollectableEntity.extend({
+var BootEntity = me.CollectableEntity.extend({
   init: function(x, y, settings) {
     this.parent(x, y, settings);
   },
